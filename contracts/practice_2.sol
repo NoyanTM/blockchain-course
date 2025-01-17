9eb76c0e814fcdf8f9afdedf0965b3bf37ae015a
NoyanTM <doge.channel@gmail.com>
2025-01-17 18:08:44 +0500

    feat: add lab 2

---
+++ contracts/practice_2.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract DataTypes {
+    // bool public myBool = true; // state
+
+    // function myFunc(bool _inputBool) public {
+    //     bool localBool = false; // local
+    // }
+
+    uint public myUnit = 42;
+    // function demo(uint _inputUint) public {
+    //     uint localUint = 42;
+    // }
+    uint8 public mySmallUint = 2;
+    // 2 ** 8 = 256
+    // 0 --> (256 - 1)
+
+    int public myInt = -42;
+    int8 public mySmallInt = -1;
+    // 2 ** 7 = 128
+    // -128  --> (128 - 1)
+
+    uint public minimum;
+    uint public maximum;
+    function calculateMinimum() public {
+        minimum = type(uint8).min; //0 
+    }
+    function calculateMaximum() public {
+        maximum = type(uint8).max;
+    }
+}
